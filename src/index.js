f1ffa155765e221403785c6aa4c859b93f49157e
martinMorkved <38659462+martinMorkved@users.noreply.github.com>
2020-09-02 08:37:13 +0200

    Add files via upload

---
+++ src/index.js
@@ -0,0 +1,20 @@
+//Create a react app from scratch.
+import React from "react";
+import ReactDOM from "react-dom";
+
+//It should display a h1 heading.
+
+ReactDOM.render(
+  <div>
+    <h1>My to do list</h1>
+    <ul>
+      <li>Code</li>
+      <li>WHATS IN THE BOX</li>
+      <li>NAV ringer kl 12:00</li>
+    </ul>
+  </div>,
+  document.getElementById("root")
+);
+
+//It should display an unordered list (bullet points).
+//It should contain 3 list elements.
